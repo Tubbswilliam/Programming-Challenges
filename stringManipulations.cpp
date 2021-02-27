72ad5fddf58ad422ac17a92ed7af2c9225fa3d1c
Tubbswilliam <williamwambua58@gmail.com>
2021-02-27 23:21:43 +0300

    More on strings

--- stringManipulations.cpp
+++ stringManipulations.cpp
@@ -1,6 +1,23 @@
-string manipulations
+//string manipulations
 #include<bits/stdc++.h>
 using namespace std;
+//Example
+vector<vector<int>> group_identical_strings(vector<string> const& s) {
+	int n = s.size();
+	vector<pair<long long, int>> hashes(n);
+	for (int i = 0; i < n; i++)
+		hashes[i] = {compute_hash(s[i]), i};
+
+	sort(hashes.begin(), hashes.end());
+
+	vector<vector<int>> groups;
+	for (int i = 0; i < n; i++) {
+		if (i == 0 || hashes[i].first != hashes[i - 1].first)
+			groups.emplace_back();
+		groups.back().push_back(hashes[i].second);
+	}
+	return groups;
+}
 int main() {
 	long long compute_hash(string const & s) {
 		const int p = 31;
