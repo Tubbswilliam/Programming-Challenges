6a2abaebce980a1d4e63ff937a20567509b3048a
Tubbswilliam <williamwambua58@gmail.com>
2021-03-07 08:32:56 +0300

    LeetCode Linked list problem

---
+++ LeetCode1290.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	int getDecimalValue(ListNode* head) {
+		int result = head->val;
+		while (head->next) {
+			result = result * 2 + head->next->val;
+			head = head->next;
+		}
+		return result;
+	}
+};
+
+int main() {return 0;}
