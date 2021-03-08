ffa248c3a0f7e07b631f0a797f66e743840be292
Tubbswilliam <williamwambua58@gmail.com>
2021-03-08 22:52:05 +0300

    Trying to revise

---
+++ RecheckingDisjointSets.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void make_set(int v) {
+	parent[v] = v;
+}
+
+int find_set(int v) {
+	if (v == parent[v])
+		return v;
+	return find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+	a = find_set(a);
+	b = find_set(b);
+	if (a != b)
+		parent[b] = a;
+}
+
+int main() {return 0;}
