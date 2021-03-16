ff15b452e0a652aca5790dde7385198474612a90
Tubbswilliam <williamwambua58@gmail.com>
2021-03-16 22:04:31 +0300

    Complete Binary Search tree

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -8,16 +8,42 @@ struct Node {
 };
 
 void bst() {
-	int n, item;
+	int n, i, item;
 	Node*p, *q, *root;
 	cout << "Enter the number of nodes." << endl;
 	cin >> n;
-	for (int i = 0; i < n; i++) {
+	for (i = 0; i < n; i++) {
 		p = new Node;
 		cin >> item;
 		p->data = item;
 		p->left = NULL;
 		P->right = NULL;
+
+		if (i == 0)root = p;
+		else {
+			q = root;
+			while (1) {
+				if (p->data > q->data)
+				{
+					if (q->right == NULL) {
+						q->right = p;
+						break;
+					}
+					else {
+						q = q->right;
+					}
+				}
+				else {
+					if (q->left == NULL) {
+						q->left = p;
+						break;
+					}
+					else {
+						q = q->left;
+					}
+				}
+			}
+		}
 	}
 }
 
