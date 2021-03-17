429561aa5ac475bbf4866904c02c12e99cddd8b4
Tubbswilliam <williamwambua58@gmail.com>
2021-03-17 23:51:46 +0300

    Linear algebra Gaussian-Elimination

--- Gaussian-Elimination.cpp
+++ Gaussian-Elimination.cpp
@@ -42,3 +42,39 @@ int main()
         }
     }
 
+    /* performing Gaussian elimination */
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            float f = mat[j][i] / mat[i][i];
+            for (k = 0; k < n + 1; k++)
+            {
+                mat[j][k] = mat[j][k] - f * mat[i][k];
+            }
+        }
+    }
+
+    /* Backward substitution for discovering values of unknowns */
+    for (i = n - 1; i >= 0; i--)
+    {
+        res[i] = mat[i][n];
+
+        for (j = i + 1; j < n; j++)
+        {
+            if (i != j)
+            {
+                res[i] = res[i] - mat[i][j] * res[j];
+            }
+        }
+        res[i] = res[i] / mat[i][i];
+    }
+
+    cout << "\nThe values of unknowns for the above equations=>\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << res[i] << "\n";
+    }
+
+    return 0;
+}
