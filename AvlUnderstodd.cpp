ab5e642a7441b797ef1ecaf3a5849003ab2ed321
Tubbswilliam <williamwambua58@gmail.com>
2021-03-18 10:55:30 +0300

    Understanding the AVL further

--- AvlUnderstodd.cpp
+++ AvlUnderstodd.cpp
@@ -1,10 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
-void avlTree() {
-	cout << "I now know how to carry out an AVL insertion!" << endl;
+//As usual we have to create the tree
+struct Node {
+	int data;
+	Node*left;
+	Node*right;
+};
+//Function to create a new node
+Node* newNode(int key) {
+	Node* node = new Node();
+	node->key = key;
+	node->left = NULL;
+	node->right = NULL;
+	node->height = 1;
+	return node;
 }
+
+//Functio to get the height of the tree
+int height(Node*N) {
+	if (N == NULL)return 0;
+	return N->height;
+}
+//Function to get the maximum between nodes
+int max(int a, int b) {
+	return a < b ? b : a;
+}
+
+//Next step I will implement the rotations
+
 int main() {
 
-	avlTree();
 	return 0;
 }
