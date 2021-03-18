e97c40934925f7d0b5f6f13c2d6db8b83372e9e8
Tubbswilliam <williamwambua58@gmail.com>
2021-03-18 11:19:49 +0300

    Avl Rotate functions&Balancing factor

--- AvlUnderstodd.cpp
+++ AvlUnderstodd.cpp
@@ -27,6 +27,46 @@ int max(int a, int b) {
 }
 
 //Next step I will implement the rotations
+//Right rotation
+Node* rightRotate(Node *y) {
+	Node*x = y->left;
+	Node*T2 = x->right;
+	//Lets rotate
+	x->right = y;
+	y->left = T2;
+
+	//updating the new height
+	y->height = max(height(y->left), height(y->right)) + 1;
+	x->height = max(height(x->left), height(x->right)) + 1;
+
+	//return the new root after rotation
+	return x;
+}
+
+//left rotation
+Node* leftRotate(Node* x) {
+	Node* y = x->right;
+	Node*T2 = y->left;
+	//rotate
+	y->left = x;
+	x->left = T2;
+
+	//updating the new heights
+	x->height = max(height(x->left), height(x->right)) + 1;
+	y->height = max(height(y->left), height(y->right)) + 1;
+
+	//returning the new root
+	return y;
+}
+
+//implement the balancng factor
+int getBalance(Node* N) {
+	if (N == NULL)return 0;
+	return height(N->left) - height(N->right);
+}
+
+//Implement a helper function to insert a new node
+
 
 int main() {
 
