5afad47a307eae1660db608cbd34c1ce67d58760
Tubbswilliam <williamwambua58@gmail.com>
2021-03-26 21:10:38 +0300

    Trying array challenge on Hackearth

--- HackerEarthArray.cpp
+++ HackerEarthArray.cpp
@@ -1,6 +1,50 @@
+/*
 #include<bits/stdc++.h>
 using namespace std;
 
+void reverseArray(int arr[], int start,
+                  int end)
+{
+	while (start < end)
+	{
+		swap(arr[start], arr[end]);
+		start++;
+		end--;
+	}
+}
+
+/* Function to right rotate arr[]
+of size n by d
+void rightRotate(int arr[], int d, int n)
+{
+	reverseArray(arr, 0, n - 1);
+	reverseArray(arr, 0, d - 1);
+	reverseArray(arr, d, n - 1);
+}
+/* function to print an array
+void printArray(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+		std::cout << arr[i] << " ";
+}
+
+int main() {
+	int A[] = {1, 2, 3, 4, 5, 6};
+
+	int n = sizeof(A) / sizeof(A[0]);
+	int k = 2;
+
+	rightRotate(A, k, n);
+	printArray(A, n);
+
+	return 0;
+}
+
+*/
+
+//The hackerEarth solution on Monk and Rotation
+#include<bits/stdc++.h>
+using namespace std;
 void reverseArray(int arr[], int start,
                   int end)
 {
@@ -20,7 +64,7 @@ void rightRotate(int arr[], int d, int n)
 	reverseArray(arr, 0, d - 1);
 	reverseArray(arr, d, n - 1);
 }
-/* function to print an array */
+/* function to print an array*/
 void printArray(int arr[], int size)
 {
 	for (int i = 0; i < size; i++)
@@ -28,13 +72,33 @@ void printArray(int arr[], int size)
 }
 
 int main() {
-	int A[] = {1, 2, 3, 4, 5, 6};
-
-	int n = sizeof(A) / sizeof(A[0]);
-	int k = 2;
-
-	rightRotate(A, k, n);
-	printArray(A, n);
-
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, k;
+		int arr[n];
+		for (int i = 0; i < n; i++) {
+			arr[i];
+		}
+		rightRotate(arr, k, n);
+		printArray(arr, n);
+	}
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
