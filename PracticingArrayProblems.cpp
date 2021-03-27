cd8afb61a089d6b6cfb3289dbba1c6314d77ca68
Tubbswilliam <williamwambua58@gmail.com>
2021-03-27 19:05:46 +0300

    Left Rotating array by one

---
+++ PracticingArrayProblems.cpp
@@ -0,0 +1,48 @@
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//largest element in array
+	int n;
+	cin >> n;
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	cout << arr[n - 1];
+	return 0;
+}
+
+*/
+
+//Function to left rotate an array one at a time
+
+#include<bits/stdc++.h>
+using namespace std;
+void leftRotateOne(int arr[], int n) {
+	int temp = arr[0];
+	for (int i = 0; i < n - 1; i++) {
+		arr[i] = arr[i + 1];
+	}
+	arr[n - 1] = temp;
+}
+void printArray(int arr[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	leftRotateOne(arr, n);
+	printArray(arr, n);
+
+	return 0;
+}
