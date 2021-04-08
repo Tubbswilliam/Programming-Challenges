2908eae9e4144e3027b84bba62021af158416f30
Tubbswilliam <williamwambua58@gmail.com>
2021-04-08 18:31:05 +0300

    Modular Exponentiation

--- FastExponentiation.cpp
+++ FastExponentiation.cpp
@@ -1,5 +1,6 @@
+/*
 //This algorithm will reduce the time complexity from o(N)
-//to log(N)
+//to log(N )
 #include<bits/stdc++.h>
 using namespace std;
 int fastExpo(int num, int pow) {
@@ -19,5 +20,31 @@ int main() {
 	int n, pow;
 	cin >> n >> pow;
 	cout << "The exponentiation of the number is: " << fastExpo(n, pow);
+	return 0;
+}
+
+*/
+
+
+//Modular exponentiation
+#include<bits/stdc++.h>
+using namespace std;
+int modExpo(int num, int pow, int mod) {
+	if (pow == 0) {
+		return 1;
+	}
+	else if (pow == 1)return num;
+	else {
+		int R = modExpo(num, pow / 2, mod);
+		if (pow % 2 == 0)return  (R * R) % mod;
+		else
+			return (R * num * R) % mod;
+	}
+}
+
+int main() {
+	int n, pow, mod;
+	cin >> n >> pow >> mod;
+	cout << "The exponentiation of the number is: " << modExpo(n, pow, mod);
 	return 0;
 }
