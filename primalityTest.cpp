7bbda208dbf93804cb716e38869d312235295d1f
Tubbswilliam <williamwambua58@gmail.com>
2021-04-11 15:42:17 +0300

    Check if a number is prime in o(sqrt(n))

--- primalityTest.cpp
+++ primalityTest.cpp
@@ -1,5 +1,5 @@
 //This prmality test works in o(n)
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 bool prime(int n) {
 	if (n == 1)return false;
@@ -13,5 +13,27 @@ int main() {
 	int n;
 	cin >> n;
 	cout << boolalpha << prime(n);
+	return 0;
+}
+*/
+
+//Using the theorem that a divisor pair(a,b)has one of the divisors
+//below the sqrt(n)and the other one above sqrt(n), we can improve the
+//time complexity of the algorithm to O(sqrt(n))
+
+#include<bits/stdc++.h>
+using namespace std;
+bool prime(int n) {
+	if (n == 1)return false;
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0)return false;
+	}
+	return true;
+}
+int main() {
+	int n;
+	cin >> n;
+	cout << boolalpha << prime(n);
+
 	return 0;
 }
