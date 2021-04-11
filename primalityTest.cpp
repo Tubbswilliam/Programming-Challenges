542415bf23f8bb281d580c4f47164c6c8cd1af52
Tubbswilliam <williamwambua58@gmail.com>
2021-04-11 14:36:22 +0300

    Check if a number is prime in o(n)

---
+++ primalityTest.cpp
@@ -0,0 +1,17 @@
+//This prmality test works in o(n)
+#include<bits/stdc++.h>
+using namespace std;
+bool prime(int n) {
+	if (n == 1)return false;
+	for (int i = 2; i < n; i++) {
+		if (n % i == 0)return false;
+	}
+	return true;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << boolalpha << prime(n);
+	return 0;
+}
