89052b48300a2a4f11f1036179c4b441c6ca13f0
Tubbswilliam <williamwambua58@gmail.com>
2021-04-12 12:49:29 +0300

    Sieve of eratosthenes challenge with an improved complexity

--- SieveOfEratosthenes.cpp
+++ SieveOfEratosthenes.cpp
@@ -1,3 +1,4 @@
+/*
 //The time complexity for the sieve eratosthene algorithm is
 //O(nlog(log(n)))
 #include<bits/stdc++.h>
@@ -18,8 +19,39 @@ int main() {
 
 	return 0;
 }
+*/
 
 
+//Spoj...Kth prime number given a query
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>primes;
+bool ar[900000001];
+void sieve() {
+	int maxN = 90000000;
+	ar[0] = ar[1] = true;
+	for (int i = 2; i * i <= maxN; i++) {
+		if (!ar[i]) {
+			for (int j = i * i; j <= maxN; j += i)
+				ar[j] = true;
+		}
+	}
+	for (int i = 2; i <= maxN; i++) {
+		if (!ar[i])
+			primes.push_back(i);
+	}
+}
+int main() {
+	int n, q;
+	cin >> q;
+	sieve();
+	while (q--) {
+		cin >> n;
+		cout << primes[n - 1] << endl;
+	}
+	return 0;
+
+}
 
 
 
