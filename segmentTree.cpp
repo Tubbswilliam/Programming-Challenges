a6856a5971beba56c3d8b4babdd63c08593f5bfc
Tubbswilliam <williamwambua58@gmail.com>
2021-04-15 19:17:56 +0300

    Spoj problem using a segment tree

--- segmentTree.cpp
+++ segmentTree.cpp
@@ -3,6 +3,7 @@
 //The code below is for finding the minimum value
 //Pseudocode for building the segment tree
 
+/*
 int buildTree(int si, int ss, int se) {
 	if (ss == se)
 		st[si] = arr[ss];
@@ -32,5 +33,48 @@ int query(int si, int ss, int se, int qs, int qe) {
 using namespace std;
 
 int main() {
+	return 0;
+}
+
+*/
+
+
+
+
+//The following is a practice problem from spoj
+#include<bits/stdc++.h>
+using namespace std;
+int ar[100001], st[400004];
+void buildST(int si, int ss, int se) {
+	if (ss == se) {
+		st[si] = ar[ss];
+		return;
+	}
+	int mid = (ss + se) / 2;
+	buildST(2 * si, ss, mid);
+	buildST(2 * si + 1, mid + 1, se);
+
+	st[si] = min(st[2 * si], st[2 * si + 1]);
+}
+
+int query(int si, int ss, int se, int qs, int qe) {
+	if (ss > qe || se < qs)return INT_MAX;
+	if (ss >= qs && se <= qe)return st[si];
+
+	int mid = (ss + se) / 2;
+	return min(query(2 * si, ss, mid, qs, qe), query(2 * si + 1, mid + 1, se, qs, qe));
+}
+
+int main() {
+	int n, q, l, r;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> ar[i];
+	buildST(1, 1, n);
+	cin >> q;
+	while (q--) {
+		cin >> l >> r;
+		cout << query(1, 1, n, l + 1 , r + 1 ) << endl;
+	}
 	return 0;
 }
