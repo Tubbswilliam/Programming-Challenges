1fc72edac3a7d85ef434af5f89ce8cddf4816cda
Tubbswilliam <williamwambua58@gmail.com>
2021-04-19 17:37:32 +0300

    Learning string algorithms

---
+++ StringAlgorithms.cpp
@@ -0,0 +1,13 @@
+//This file will contain the string manipulations and techniques
+//Basic Algorithms(Sliding window,string hashing/Rplling hash) and Rabin-karp matching algorithm
+//Intermediate algorithms(Prefix fn and KMP/Z-fn/Suffix array/Trie Ds)
+//Advanced algorithms(Aho-Corasick/Suffix automation/Manacher's algorithm)
+
+//1) Sliding window algorithm
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
