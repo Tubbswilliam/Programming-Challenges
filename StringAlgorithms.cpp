35b38af7257c3e9588261ac775d298da5c8324eb
Tubbswilliam <williamwambua58@gmail.com>
2021-04-19 18:07:15 +0300

    String sliding window algorithms

--- StringAlgorithms.cpp
+++ StringAlgorithms.cpp
@@ -4,10 +4,32 @@
 //Advanced algorithms(Aho-Corasick/Suffix automation/Manacher's algorithm)
 
 //1) Sliding window algorithm
+//can be used to search for a certain pattern in a string.
+
 #include<bits/stdc++.h>
 using namespace std;
+string text, pattern;
+int txt, pn;
+
+bool match(int left, int right) {
+	for (int i = 0; i < pn; i++) {
+		if (text[left + i] != pattern[i])return false;
+	}
+	return true;
+
+}
 
 int main() {
+	getline(cin, text);
+	getline(cin, pattern);
+
+	txt = text.size();
+	pn = pattern.size();
+
+	for (int left = 0, right = pn - 1; right < txt; left++, right++)
+		if (match(left, right))
+			cout << left << " " << right << endl;
+
 
 	return 0;
 }
