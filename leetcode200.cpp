1ddc8f8a5658087b7fdf78d467a8da1224419680
Tubbswilliam <williamwambua58@gmail.com>
2021-04-27 11:24:35 +0300

    Graph problem solved using DFS'

--- leetcode200.cpp
+++ leetcode200.cpp
@@ -13,6 +13,38 @@ grid are all surrounded by water.
 #include<bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+	void dfs(vector<vector<char>>& grid, int i, int j) {
+		grid[i][j] = '0';
+
+		if (i + 1 < grid.size() && grid[i + 1][j] == '1')
+			dfs(grid, i + 1, j);
+		if (i - 1 >= 0 && grid[i - 1][j] == '1')
+			dfs(grid, i - 1, j);
+		if (j + 1 < grid[0].size() && grid[i][j + 1] == '1')
+			dfs(grid, i, j + 1);
+		if (j - 1 >= 0 && grid[i][j - 1] == '1')
+			dfs(grid, i, j - 1);
+	}
+
+	int numIslands(vector<vector<char>>& grid) {
+		int m = grid.size();
+		int n = grid[0].size();
+		int count = 0;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (grid[i][j] == '1') {
+					dfs(grid, i, j);
+					count++;
+				}
+
+			}
+		}
+		return count;
+	}
+};
+
 int main() {
 #ifndef ONLINE_JUDGE
 //for getting input from input.txt
@@ -22,6 +54,8 @@ int main() {
 #endif
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
+
+
 
 	return 0;
 }
