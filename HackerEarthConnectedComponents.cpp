02d6a32828526d1b55ec2d69fab6b5b3555749d8
Tubbswilliam <williamwambua58@gmail.com>
2021-04-27 12:27:14 +0300

    Connected Components

---
+++ HackerEarthConnectedComponents.cpp
@@ -0,0 +1,24 @@
+/*
+Problem
+
+Given n, i.e. total number of nodes in an undirected graph numbered from 1 to n and an integer e,
+ i.e. total number of edges in the graph. Calculate the total number of connected components in the graph.
+A connected component is a set of vertices in a graph that are linked to each other by paths.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int main() {
+#ifndef ONLINE_JUDGE
+		//for getting input from input.txt
+		freopen("input.txt", "r", stdin);
+		// for writing output to output.txt
+		freopen("output.txt", "w", stdout);
+#endif
+		ios_base::sync_with_stdio(0);
+		cin.tie(0);
+		return 0;
+	}
