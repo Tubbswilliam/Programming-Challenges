8ffeeff19e6ef473130a2a75f4acd54e48a56b89
Tubbswilliam <williamwambua58@gmail.com>
2021-04-27 10:03:01 +0300

    Graph problem'

---
+++ leetcode200.cpp
@@ -0,0 +1,27 @@
+/*
+Number of Islands
+
+
+Given an m x n 2D binary grid grid which represents a map of '1's (land)
+and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent
+lands horizontally or vertically. You may assume all four edges of the
+grid are all surrounded by water.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+//for getting input from input.txt
+	freopen("inputBFSandDFS.txt", "r", stdin);
+// for writing output to output.txt
+	freopen("outputBFSandDFS.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	return 0;
+}
