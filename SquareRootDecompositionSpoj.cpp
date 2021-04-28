00e851a7439c0f925688b30bf63854255e9896d8
Tubbswilliam <williamwambua58@gmail.com>
2021-04-28 14:58:19 +0300

    Mo's Algorithm

--- SquareRootDecompositionSpoj.cpp
+++ SquareRootDecompositionSpoj.cpp
@@ -27,3 +27,30 @@ int getMin(int l,int r){
 }
 
 */
+
+
+
+/*
+Problem
+
+Given an array of integers and queries of the form L,R,
+how many elements exist in the given range?
+*/
+
+//Basic template of the Mo's Algorithm
+
+for (int i = 0; i < q; i++) {
+	int L = Q[i]; //l
+	int R = Q[i]; //r
+	int idx = Q[i]; //i
+
+	//extending the range
+	while (MR < R)MR++, add(MR);
+	while (ML > L)ML--, add(ML);
+
+	//reducing the range
+	while (MR > R)remove(MR), MR--;
+	while (ML < L)remove(ML), ML++;
+
+	ans[idx] = count;
+}
