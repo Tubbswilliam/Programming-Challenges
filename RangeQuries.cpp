b3e1620ba5204a3da06b025e8e4e77540a5e2e86
Tubbswilliam <williamwambua58@gmail.com>
2021-04-29 15:10:07 +0300

    Segment tree

--- RangeQuries.cpp
+++ RangeQuries.cpp
@@ -1,5 +1,5 @@
 //Fenwick Tree
-
+/*
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -35,19 +35,75 @@ int find(int k) {
 	return (curr + 1);
 }
 
+*/
+
+
+//Segment tree
+#include<bits/stdc++.h>
+using namespace std;
+
+int arr[1000001], seg[4 * 1000001];
+
+void build(int ind, int low, int high) {
+	if (low == high) {
+		seg[ind] = arr[low];
+		return;
+	}
+	int mid = (low + high) / 2;
+	build(2 * ind + 1, mid, high);
+	build(2 * ind + 2, mid + 1, high);
+	seg[ind] = max(seg[2 * ind + 1], seg[2 * ind + 2]);
+
+}
+
+int query(int ind, int low, int high, int l, int r) {
+	if (low >= l && high <= r)
+		return seg[ind];
+
+	if (high < l || r < low) {
+		return INT_MIN;
+	}
+
+	//in the case of an overLap
+	int mid = (low + high) / 2;
+	int left = query(2 * ind + 1, low, mid, l, r);
+	int right = query(2 * ind + 2, mid + 1, high, l, r);
+	return max(left, right);
+
+}
+
+
 
 int main() {
-#ifndef ONLINE_JUDGE
-	//for getting input from input.txt
-	freopen("input.txt", "r", stdin);
-	// for writing output to output.txt
-	freopen("output.txt", "w", stdout);
-#endif
+// #ifndef ONLINE_JUDGE
+// 	//for getting input from input.txt
+// 	freopen("input.txt", "r", stdin);
+// 	// for writing output to output.txt
+// 	freopen("output.txt", "w", stdout);
+// #endif
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	//Build the segment tree
+	//low is the beginning of the array and high is the end of the array
+	build(0, 0, n - 1);
 
-
-
+	//You need to query the segment tree
+	int q;
+	cin >> q;
+	while (q--) {
+		int l, r;
+		cin >> l >> r;
+		cout << query(0, 0, n - 1, l, r) << endl;
+	}
 	return 0;
 }
+
+
+
+
