6ebf13834e7a77a984cc70388eaf5beb364b5a6b
Tubbswilliam <williamwambua58@gmail.com>
2021-05-07 16:34:29 +0300

    Codeforces 719 Problem c

--- codeforces719Div3.cpp
+++ codeforces719Div3.cpp
@@ -1,7 +1,7 @@
 
 //This Round was just a practice round for me
 //Problem A- Do Not be distracted
-
+/*
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -17,6 +17,48 @@ void solve() {
 		cout << "YES" << endl;
 	else
 		cout << "NO" << '\n';
+}
+int main() {
+#ifndef ONLINE_JUDGE
+	//for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+	// for writing output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+
+
+
+
+	return 0;
+}
+
+*/
+
+//Problem c:Not adjacent Matrix
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	int current = 1;
+	int n;
+	cin >> n;
+	if (n == 2)cout << "-1" << endl;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << current << " " << endl;
+			current += 2;
+			if (current > n * n)
+				current = 2;
+		}
+	}
 }
 int main() {
 #ifndef ONLINE_JUDGE
