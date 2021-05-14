a5c525ad063361385314edd745e717eced6e1b77
Tubbswilliam <williamwambua58@gmail.com>
2021-05-14 22:12:52 +0300

    Tree Centroid

---
+++ TreeAlgorithms.cpp
@@ -0,0 +1,18 @@
+//Finding a centroid
+//A centroid is a point that when removed the created connected component component
+//doesn't have size greater than 2 where N is size of the tree.
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+#ifndef ONLINE_JUDGE
+//for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+	// for writing output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	return 0;
+}
