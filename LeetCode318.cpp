d6623f434b020e22451290ca1501dafe3e86f545
Tubbswilliam <williamwambua58@gmail.com>
2021-05-30 12:49:57 +0300

    Finalizing on the question

--- LeetCode318.cpp
+++ LeetCode318.cpp
@@ -14,9 +14,19 @@ int maxProduct(vector<string>&words) {
 		words_int[i] = w_int;
 		word_len[i] = _len;
 	}
-
-
-	int main() {
-		maxProduct();
-		return 0;
+	int max_product = 0;
+	for (int i = 0; i < n - 1; ++i) {
+		for (int j = i + 1; j < n; ++j) {
+			if ((word_int[i]&word_int[j]) == 0)
+				max_product = max(ma_product, word_len[i] * word_len[j]);
+		}
 	}
+	return max_product;
+}
+
+
+
+int main() {
+	maxProduct();
+	return 0;
+}
