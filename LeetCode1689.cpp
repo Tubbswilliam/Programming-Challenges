409213912e6cefe9c6f19973b434db00960284ab
Tubbswilliam <williamwambua58@gmail.com>
2021-06-01 09:41:26 +0300

    deeci-binary number code

--- LeetCode1689.cpp
+++ LeetCode1689.cpp
@@ -8,7 +8,16 @@ return the minimum number of positive deci-binary numbers needed so that they su
 #include<bits/stdc++.h>
 using namespace std;
 
-
+class Solution {
+public:
+    int minPartitions(string n) {
+        int largestNumber = 0;
+        for (int i = 0; i < n.length(); i++) {
+            largestNumber = max(largestNumber, n[i] - '0');
+        }
+        return largestNumber;
+    }
+};
 int main() {
     return 0;
 }
