266ded213e93895fa503f7d6fedc545c9b51d454
Tubbswilliam <williamwambua58@gmail.com>
2021-06-01 09:27:07 +0300

    deeci-binary number

---
+++ LeetCode1689.cpp
@@ -0,0 +1,14 @@
+//Partitioning Into Minimum Number Of Deci-Binary Numbers
+
+/*
+Given a string n that represents a positive decimal integer,
+return the minimum number of positive deci-binary numbers needed so that they sum up to n.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    return 0;
+}
