6b6a16f542277f08230c90cff867197fc119a31b
Tubbswilliam <williamwambua58@gmail.com>
2021-06-02 08:39:27 +0300

    Binary search functions

--- BinarySearchFunctions.cpp
+++ BinarySearchFunctions.cpp
@@ -9,6 +9,9 @@ int main() {
 	vector<int>ar = {1, 1, 2, 2, 2, 4, 7, 10};
 	int br[] = {2, 3, 5, 7};
 
-	cout << binary_search(ar.begin(), ar.end(), 3);
+	cout << binary_search(ar.begin(), ar.end(), 3) << endl;
+	cout << binary_search(br, br + 4, 5) << endl;
+
+	cout << *upper_bound(ar.begin(), ar.end(), 2) << endl;
 	return 0;
 }
