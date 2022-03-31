5a5efe188b0675941d578055968d208df2fc33ef
Tubbswilliam <williamwambua58@gmail.com>
2022-03-31 11:26:06 +0300

    Inheritance in python

--- Trees_in_Python.py
+++ Trees_in_Python.py
@@ -49,3 +49,59 @@ Student.school="MIT" #Changing class variable
 s2.name="Wambua" #changing instance variable
 print(s1.name+" "+s1.school)
 print(s2.name+" "+s2.school)
+
+
+###################################################################################
+class Student:
+    def __init__(self,name,rollno):
+        self.name=name
+        self.rollno=rollno
+        self.laptop=self.Laptop()
+        
+    def show(self):
+        print(self.name+" "+self.rollno)
+        self.laptop.show()
+        
+    class Laptop:
+        def __init__(self):
+            self.brand="Hp"
+            self.cpu='i5'
+            self.ram=64
+            
+        def show(self):
+            print(self.brand+" "+self.cpu,self.ram)
+        
+s1=Student("William","20")
+s2=Student("Wambua","39")
+
+s1.show()
+
+#########################################################################################
+#inheritance
+#A is super class and B is sub-Class 
+class A:
+    def feature1(self):
+        print("Parent class")
+        
+    def feature2(self):
+        print("Another parent class")
+        
+class B(A):
+        def feature3(self):
+            print("child class")
+        def feature4(self):
+            print("Another child class")
+    
+
+        
+        
+a1=A()
+a1.feature1()
+
+a2=B()
+a2.feature3()
+
+#inheritrance
+a2.feature1()
+
+#############################################################################################
